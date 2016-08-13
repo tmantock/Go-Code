6f007dcbb0b484549ef2e9de23fa18863cb0f72e
Tevin Mantock <tmantock@gmail.com>
2016-08-12 21:48:52 -0700

    testing loop scope

--- practice/02_numeral-systems/05_utf8/main.go
+++ practice/02_numeral-systems/05_utf8/main.go
@@ -6,4 +6,10 @@ func main() {
 	for i := 0; i < 200; i++ {
 		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
 	}
+	//i is undefined when trying to print outside of the loop
+	//fmt.Println(i)
+	//See if i is within the scope of the loo[ or function]
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
+	}
 }
