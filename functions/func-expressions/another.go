0a31bde087678a7422fe98f6776be4d45e944d8f
Tevin Mantock <tmantock@gmail.com>
2016-09-03 22:42:44 -0700

    callbacks

---
+++ functions/func-expressions/another.go
@@ -0,0 +1,14 @@
+package another
+
+import "fmt"
+
+func makeGreeter() func() string {
+	return func() string {
+		return "Hello World!"
+	}
+}
+
+func main() {
+	greet := makeGreeter()
+	fmt.Println(greet())
+}
