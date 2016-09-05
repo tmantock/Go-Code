4399abe5546eeac37e422123bb5225f9c31540ba
Tevin Mantock <tmantock@gmail.com>
2016-09-04 22:28:11 -0700

    bytes and ints and arrays

--- arrays/main.go
+++ arrays/main.go
@@ -13,4 +13,30 @@ func main() {
 	fmt.Println(x)
 	fmt.Println(len(x))
 	fmt.Println(x[42])
+
+	var y [256]int
+	fmt.Println(len(y))
+	fmt.Println(y[42])
+	for i := 0; i < 256; i++ {
+		y[i] = i
+	}
+	for i, v := range y {
+		fmt.Printf("%v - %T - %b\n", v, v, v)
+		if i > 50 {
+			break
+		}
+	}
+
+	var w [256]byte
+	fmt.Println(len(w))
+	fmt.Println(w[42])
+	for i := 0; i < 256; i++ {
+		w[i] = byte(i)
+	}
+	for i, v := range w {
+		fmt.Printf("%v - %T - %b\n", v, v, v)
+		if i > 50 {
+			break
+		}
+	}
 }
