5ea436f5299e9d43ea4f718b72f275be53f57b0b
Tevin Mantock <tmantock@gmail.com>
2016-09-24 15:42:20 -0700

    comma ok idiom and maps

---
+++ maps/comma-ok-idiom/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	myGreeting := map[int]string{
+		0: "Good morning!",
+		1: "Bonjour!",
+		2: "Buenos dias!",
+		3: "Bongiorno!",
+	}
+
+	fmt.Println(myGreeting)
+
+	//delete(myGreeting, 2)
+
+	if val, exists := myGreeting[2]; exists {
+		delete(myGreeting, 2)
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+	} else {
+		fmt.Println("That value does not exist")
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+	}
+	fmt.Println(myGreeting)
+}
