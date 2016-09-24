4552619b9e21af3b47b70b25fb071514128ed686
Tevin Mantock <tmantock@gmail.com>
2016-09-24 15:35:15 -0700

    delete entry from map

---
+++ maps/delete-entry/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	myGreeting := map[int]string{
+		0: "Good morning!",
+		1: "Bonjour!",
+		2: "Buenos dias!",
+		3: "Bongiorno!",
+	}
+
+	fmt.Println(myGreeting)
+	delete(myGreeting, 2)
+	fmt.Println(myGreeting)
+}
