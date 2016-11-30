3b623410ed31f6a456163da1aca1f56a4a676d40
Tevin Mantock <tmantock@gmail.com>
2016-11-29 22:05:55 -0800

    more structs

--- structs/fields-values-initialization/main.go
+++ structs/fields-values-initialization/main.go
@@ -8,9 +8,26 @@ type person struct {
 	age   int
 }
 
+type foo int
+
 func main() {
 	p1 := person{"James", "Bond", 25}
 	p2 := person{"Sly", "Cooper", 18}
+
+	var myage foo
+	myage = 23
+	fmt.Printf("%T %v \n", myage, myage)
+
+	var yourage int
+	yourage = 44
+	fmt.Printf("%T %v \n", yourage, yourage)
+
+	//This will not work
+	//fmt.Println(myage + yourage)
+
+	//This will work
+	fmt.Println(int(myage) + yourage)
+
 	fmt.Println(p1.first, p1.last, p1.age)
 	fmt.Println(p2.first, p2.last, p2.age)
 }
