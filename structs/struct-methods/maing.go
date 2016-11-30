d3daac2c8fb4a4ffb16e7fb52b4bb530fbdd12a2
Tevin Mantock <tmantock@gmail.com>
2016-11-29 22:16:39 -0800

    struct methods

---
+++ structs/struct-methods/maing.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+func (p person) fullName() string {
+	return p.first + p.last
+}
+func main() {
+	p1 := person{"James", "Bond", 25}
+	p2 := person{"Sly", "Cooper", 18}
+	fmt.Println(p1.fullName())
+	fmt.Println(p2.fullName())
+}
