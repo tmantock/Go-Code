74677621dfed3c4950182b894789662656c8ee08
Tevin Mantock <tmantock@gmail.com>
2016-12-07 21:22:46 -0800

    sorting with interfaces

---
+++ sort/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type people []string
+
+func (p people) Len() int           { return len(p) }
+func (p people) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p people) Less(i, j int) bool { return p[i] < p[j] }
+
+func main() {
+	studyGroup := people{"Zeno", "John", "Al", "Jenny"}
+
+	s := []string{"Zeno", "John", "Al", "Jenny"}
+	fmt.Println(s)
+	sort.StringSlice(s).Sort()
+	//sort.Sort(StringSlice(s))
+	fmt.Println(s)
+
+	fmt.Println(studyGroup)
+	sort.Sort(studyGroup)
+	fmt.Println(studyGroup)
+}
